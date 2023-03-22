040cc01af8664fa69c8ae5928cb57cfa1dc3ba24
Antonio2254 <antonioluna486@gmail.com>
2023-03-21 23:01:34 -0500

    JOIN EN TABLA ORDER DETAIL
    Apetición de Front, se cambio la consulta GET en la Tabla Order_detail.
    
    - Se agrego la sentencia JOIN para mostrar el producto relacionado al id en la tabla Order_detail
    
    Ahora en lugar de mostrar solo el Id del producto, mostrara el nombre del producto con respecto al ID.
    
    Esto se agrego al de todas las Order_detail, como al de una sola order.
    
    Co-Authored-By: Carrillo5308 <123425177+Carrillo5308@users.noreply.github.com>

--- clases/order_detail.php
+++ clases/order_detail.php
@@ -1,5 +1,7 @@
 <?php
 namespace clases; //NO EDITAR//
+use Firebase\JWT\JWT; //NO EDITAR//
+USE Firebase\JWT\Key; //NO EDITAR//
 require 'vendor/autoload.php'; //NO EDITAR//
 use Flight; //NO EDITAR//
 
@@ -11,15 +13,66 @@ class order_detail
     //Función constructor CONEXIÓN A BASE DE DATOS, No modificar el DB//
     function __construct()
     {
-        Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=ecommerce_unid','root',''));
+        Flight::register('db', 'PDO', array('mysql:host='.$_ENV['db_host'].';dbname='. $_ENV['db_name'],$_ENV['db_user'],$_ENV['db_pass']));
         $this->db = Flight::db();
     }
 
+    //Funcion Token users//
+    function getToken()
+    {
+        $header = apache_request_headers();
+        if (!isset($header["Authorization"]))
+        {
+            Flight::halt(403, json_encode([
+            "error" => 'unauthenticated request',
+            "status" => 'error'
+            ]));            
+        }
+        $authorization = $header["Authorization"];
+        $authorizationArray = explode(" ", $authorization);
+        $token = $authorizationArray[1];
+        $key = $_ENV['user_key'];
+        try{
+            return JWT::decode($token, new key($key, 'HS256'));
+        }
+        catch(\Throwable $th){
+            Flight::halt(403, json_encode([
+                "error" => $th ->getMessage(),
+                "status" => 'error'
+            ]));
+        }
+        return $token;
+    }
+     
+    //Funcion Validar Token users//
+    function validateToken()
+    {
+        $info = $this->getToken();
+        $db= Flight::db();
+        $query = $db->prepare("SELECT * FROM users where id = :id");            
+        $query->execute([":id"=>$info->data]);
+        $rows = $query->fetchColumn();
+        return $rows;
+    }
 
     //Función select all order_detail//
     function selectall_order_detail()
     {
-        $query = $this->db->prepare("SELECT * FROM order_detail");
+         ////////Join de los Product_id de la tabla order_detail con el campo Product_name de la tabla Products//////////////
+        $query = $this->db->prepare("SELECT 
+        p.product_name AS 'product',
+        od.id AS 'id',
+        od.order_id AS 'order_id',
+        od.image AS 'image',
+        od.quantity AS 'quantity',
+        od.price AS 'price',
+        od.creation_date AS 'creation_date'
+        FROM order_detail od 
+        JOIN products p 
+        ON p.id = od.product_id;");
+        ////////Termina el Join de los product_id con el campo Product_name//////////////
+
+
         $query->execute();
         $data = $query->FetchAll();
     
@@ -28,11 +81,12 @@ class order_detail
         {
             $array[] = [
                 'id' => $row['id'],
+                'product' => $row['product'],
                 'order_id' => $row['order_id'],
-                'product_id' => $row['product_id'],
                 'image' => $row['image'],
                 'quantity' => $row['quantity'],
                 'price' => $row['price'],
+                'creation_date' => $row['creation_date'],
             ];
         }
         Flight::json([
@@ -45,17 +99,31 @@ class order_detail
     //Función select single order_detail//
     function selectone_order_detail($id)
     {
-        $query = $this->db->prepare("SELECT * FROM order_detail WHERE id = :id");
+        ////////Join del product_id de la tabla order_detail con el campo Product_name de la tabla Products//////////////
+        $query = $this->db->prepare("SELECT 
+        p.product_name AS 'product',
+        od.id AS 'id',
+        od.order_id AS 'order_id',
+        od.image AS 'image',
+        od.quantity AS 'quantity',
+        od.price AS 'price',
+        od.creation_date AS 'creation_date'
+        FROM order_detail od 
+        JOIN products p 
+        ON p.id = od.product_id WHERE od.id = :id");
+        ////////Termina el Join del product_id con el campo Product_name//////////////
+
         $query->execute([":id" => $id]);
         $data = $query->Fetch();
     
         $array = [
             'id' => $data['id'],
+            'product' => $data['product'],
             'order_id' => $data['order_id'],
-            'product_id' => $data['product_id'],
             'image' => $data['image'],
             'quantity' => $data['quantity'],
             'price' => $data['price'],
+            'creation_date' => $data['creation_date'],
         ];  
         Flight::json($array);
     }
@@ -64,21 +132,36 @@ class order_detail
     //Función insert order_detail//
     function order_detail_post()
     {
+        if(!$this->validateToken())
+        {
+            Flight::halt(403, json_encode([
+                "error" => 'Unauthorized',
+                "status" => 'error'
+            ]));
+        }
+        //ESTA ES LA EDICIÓN DEL PROFESOR//
+        $body = Flight::request()->getBody();
+        $data = json_decode($body);
         $db = Flight::db();
-        $order_id  = Flight::request()->data->order_id ;
-        $product_id = Flight::request()->data->product_id;
-        $image = Flight::request()->data->image;
-        $quantity = Flight::request()->data->quantity;
-        $price = Flight::request()->data->price;
-    
-        $query = $db->prepare("INSERT INTO order_detail (order_id, product_id, image, quantity, price) VALUES (:order_id,:product_id, :image, :quantity, :price)");
+
+        $order_id  = $data->order_id;
+        $product_id = $data->product_id;
+        $image = $data->image;
+        $quantity = $data->quantity;
+        $price = $data->price;
+        $creation_date = $data->creation_date;
+        //AQUI FINALIZA LA EDICIÓN DEL PROFESOR//
+        
+        $query = $db->prepare("INSERT INTO order_detail (order_id, product_id, image, quantity, price, creation_date) 
+        VALUES (:order_id, :product_id, :image, :quantity, :price,  :creation_date)");
     
         $array = [
         "error" => "Hubo un error al agregar los registros, por favor intenta mas tarde",
         "status" => "error" 
         ];
 
-        if($query->execute([":order_id" => $order_id, ":product_id" => $product_id, ":image" => $image, ":quantity" => $quantity, ":price" => $price]))
+        if($query->execute([":order_id" => $order_id, ":product_id" => $product_id, ":image" => $image, 
+        ":quantity" => $quantity, ":price" => $price, ":creation_date" => $creation_date]))
         {
             $array = [
                 "data" => [
@@ -89,6 +172,7 @@ class order_detail
                     'image' => $image,
                     'quantity' => $quantity,
                     'price' => $price,
+                    'creation_date' => $creation_date,
                 ],
                 "status" => "success"
             ];
@@ -100,30 +184,48 @@ class order_detail
     //Función put order_detail//
     function order_detail_put()
     {
+        if(!$this->validateToken())
+        {
+            Flight::halt(403, json_encode([
+                "error" => 'Unauthorized',
+                "status" => 'error'
+            ]));
+        }
+        //ESTA ES LA EDICIÓN DEL PROFESOR//
+        $body = Flight::request()->getBody();
+        $data = json_decode($body);
         $db = flight::db();
-        $id = flight::request()->data->id;
-        $order_id = flight::request()->data->order_id;
-        $product_id = flight::request()->data->product_id;
-        $quantity = flight::request()->data->quantity;
-        $price = flight::request()->data->price;
+
+        $id = $data->id;
+        $order_id = $data->order_id;
+        $product_id = $data->product_id;
+        $image = $data->image;
+        $quantity = $data->quantity;
+        $price = $data->price;
+        $creation_date = $data->creation_date;
+        //AQUI FINALIZA LA EDICIÓN DEL PROFESOR//
 
         
-        $query = $db->prepare("UPDATE order_detail SET order_id = :order_id, product_id = :product_id, quantity = :quantity, price = :price WHERE id = :id ");
+        $query = $db->prepare("UPDATE order_detail SET order_id = :order_id, product_id = :product_id, image = :image,
+         quantity = :quantity, price = :price, creation_date = :creation_date WHERE id = :id ");
        
         $array = [
         "error" => "Hubo un error al agregar los registros, por favor intenta mas tarde",
         "status" => "error" 
         ];
     
-        if ($query->execute([ ":order_id" => $order_id, ":product_id" => $product_id, ":quantity" => $quantity, ":price" => $price, ":id" => $id]))
+        if ($query->execute([ ":order_id" => $order_id, ":product_id" => $product_id, ":image" => $image, 
+        ":quantity" => $quantity, ":price" => $price, ":creation_date" => $creation_date, ":id" => $id]))
         {
             $array = [
                 "data" => [
                     "id" => $id,
                     "order_id" => $order_id,
                     "product_id" => $product_id,
+                    "image" => $image,
                     "quantity" => $quantity,
                     "price" => $price,
+                    "creation_date" => $creation_date,
                 ],
                 "status" => "success"
             ];
@@ -134,6 +236,13 @@ class order_detail
     //Función delete order_detail//
     function delete_order_detail()
     {
+        if(!$this->validateToken())
+        {
+            Flight::halt(403, json_encode([
+                "error" => 'Unauthorized',
+                "status" => 'error'
+            ]));
+        }
         $db = flight::db();
         $id = flight::request()->data->id;
         
